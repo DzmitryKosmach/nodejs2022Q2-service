bca4cfc61fd32489ce024135ea400920409db298
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2022-08-05 14:03:41 +0300

    init: task logging

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+const MSG_HELLO = 'Hello World!';
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return MSG_HELLO;
   }
 }
